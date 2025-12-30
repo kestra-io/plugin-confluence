737919e910b4e57301e4b5831631f155b8af5dcd
ben8t <pimpaudben@gmail.com>
2025-12-30 15:34:56 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/confluence/pages/package-info.java
+++ src/main/java/io/kestra/plugin/confluence/pages/package-info.java
@@ -2,8 +2,8 @@
     title = "Confluence Pages",
     description = "This sub-group of plugins contains tasks for accessing and managing Confluence pages.\n" +
         "Confluence pages provide a flexible way to collaborate, document, and share knowledge within teams.",
-    categories = {
-        PluginSubGroup.PluginCategory.TOOL
+        categories = {
+        PluginSubGroup.PluginCategory.BUSINESS
     }
 )
 package io.kestra.plugin.confluence.pages;
